2a8f93dcf4d9ad933309ed6f887d91fb74eb883b
shakila <skshakilask67@gmail.com>
2025-07-06 23:15:17 +0530

    creating schema

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-import express from "express"
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+dotenv.config();
 
+const app = express();
 
-const app = express()
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 8000
-
-
-app.get("/", (req, res)=> {
-  res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.listen(port, () => {
-  console.log("server started")
-})
+  connectDb();
+  console.log("server started");
+});
