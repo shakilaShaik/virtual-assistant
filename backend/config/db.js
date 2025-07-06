6729a1120a054d48795f2a6827718cbc5e68399b
shakila <skshakilask67@gmail.com>
2025-07-06 23:05:04 +0530

    configured db

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose, { modelNames } from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL)
+  }
+  catch(error)
+   {
+    console.log(error)
+  }
+}
