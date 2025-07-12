0d4122b9e23db54cc6ccda37b56b9b88e4eba5d8
shakila <skshakilask67@gmail.com>
2025-07-12 15:10:13 +0530

    done frontend for signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import singupbg from "../assets/signup-img.png";
-
 import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
   return (
     <div
       className="w-full h-[100vh]  bg-cover flex justify-center align-items-center bg-center"
       style={{
         backgroundImage: `url(${singupbg})`,
       }}>
-      <form className="w-[90%] h-[600px] max-w-[500px] bg-transparent backdrop-blur shadow-lg shadow-blue flex flex-col items-center  gap-[20px] mt-10 px-[20px]">
-        <h1 className="text-white text-[30px] font-semibold mb-[30px] mt-2">
+      <form className="w-[90%] h-[500px] max-w-[500px] bg-transparent backdrop-blur shadow-lg shadow-blue flex flex-col items-center  gap-[20px] mt-10 px-[20px]">
+        <h1 className="text-white text-[30px] font-semibold mb-[30px] mt-7">
           Register
         </h1>
 
@@ -32,15 +34,31 @@ const Signup = () => {
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Enter Your Password"
-            className="w-full h-full outline-none border-2 border-white bg-transparent text-white placeholder:text-gray-300 px-[20px] pr-[50px] rounded-full text-[18px]"
+            className="w-full h-full outline-none border-2 border-white bg-transparent text-white placeholder:text-gray-300 px-[20px] pr-[50px] rounded-full text-[18px] "
           />
-          {showPassword && (
+          {!showPassword && (
             <IoEye
               className="absolute right-[20px] top-1/2 transform -translate-y-1/2 text-white text-[22px] cursor-pointer"
               onClick={() => setShowPassword(true)}
             />
           )}
+          {showPassword && (
+            <IoEyeOff
+              className="absolute right-[20px] top-1/2 transform -translate-y-1/2 text-white text-[22px] cursor-pointer"
+              onClick={() => setShowPassword(false)}
+            />
+          )}
         </div>
+
+        <button className="min-w-[150px] h-[60px] bg-white rounded-full font-semibold text-[19px] cursor-pointer">
+          Signup
+        </button>
+        <p  onClick={()=>navigate('/signin')} className="text-white text-[18px] cursor-pointer ">
+          Already have an account?
+          <span className="text-blue-950 font-semibold cursor-pointer ">
+            Signin
+          </span>
+        </p>
       </form>
     </div>
   );
